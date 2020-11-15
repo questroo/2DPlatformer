58526a1a74b88b38b1b3561677805ad64bced86d
questroo <42195958+questroo@users.noreply.github.com>
2020-11-15 13:00:51 -0800

    - Added all the animations for the Player. - Setup the AnimationController.cs to be able to change animations but not for movement logic. - Imported a knight asset.

---
+++ Assets/Scripts/AnimatorContoller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorContoller : MonoBehaviour
+{
+    // Animation States
+    const string PLAYER_IDLE = "Player_Idle";
+    const string PLAYER_ATTACK = "Player_Attack";
+    const string PLAYER_SPELLCAST = "Player_Spellcast";
+    const string PLAYER_CROUCH = "Player_Crouch";
+    const string PLAYER_DASH = "Player_Dash";
+    const string PLAYER_JUMPATTACK = "Player_JumpAttack";
+    const string PLAYER_WALK = "Player_Walk";
+    const string PLAYER_BLOCK = "Player_Block";
+    const string PLAYER_DIZZY = "Player_Dizzy";
+    const string PLAYER_HURT = "Player_Hurt";
+    const string PLAYER_JUMP = "Player_Jump";
+    const string PLAYER_STRIKE = "Player_Strike";
+    const string PLAYER_CELEBRATION = "Player_Celebration";
+    const string PLAYER_DIE = "Player_Die";
+
+    Animator animator => GetComponent<Animator>();
+    Rigidbody2D rb => GetComponent<Rigidbody2D>();
+    private string currentState;
+    private int groundMask => 1 << LayerMask.NameToLayer("Ground");
+
+    private void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState)
+            return;
+        animator.Play(newState);
+
+        currentState = newState;
+    }
+}
