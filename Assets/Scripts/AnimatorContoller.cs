f8e686125007de28d7c0ad479956813ee5284773
questroo <42195958+questroo@users.noreply.github.com>
2020-11-16 14:07:11 -0800

    - Added "Ground" layer tag. - Changed the speed of the animations for the player from 1 to 0.2 - Implemented animations for the idle, walk, crouch, attack, jump, and jump attack.

--- Assets/Scripts/AnimatorContoller.cs
+++ Assets/Scripts/AnimatorContoller.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class AnimatorContoller : MonoBehaviour
 {
+    private bool isGrounded = true;
+    private bool isAttacking = false;
+    private bool isCrouching;
+    private bool isJumpPressed;
+    private bool isAttackPressed;
+    private float xAxis;
+    private string currentState;
+    public float moveSpeed = 4.0f;
+    public float jumpForce = 4.0f;
+    public float attackDelay = 0.3f;
+    public Transform footPosition;
+
     // Animation States
     const string PLAYER_IDLE = "Player_Idle";
     const string PLAYER_ATTACK = "Player_Attack";
@@ -21,9 +33,103 @@ public class AnimatorContoller : MonoBehaviour
     const string PLAYER_DIE = "Player_Die";
 
     Animator animator => GetComponent<Animator>();
-    Rigidbody2D rb => GetComponent<Rigidbody2D>();
-    private string currentState;
-    private int groundMask => 1 << LayerMask.NameToLayer("Ground");
+    Rigidbody2D rb2d => GetComponent<Rigidbody2D>();
+    private int groundMask;
+
+    private void Start()
+    {
+        groundMask = 1 << LayerMask.NameToLayer("Ground");
+    }
+
+    private void Update()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumpPressed = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isAttackPressed = true;
+        }
+
+        isCrouching = Input.GetKey(KeyCode.DownArrow);
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(footPosition.position, Vector2.down, 0.1f, groundMask);
+
+        isGrounded = hit.collider != null ? true : false;
+
+        Vector2 vel = new Vector2(0, rb2d.velocity.y);
+
+        if (xAxis < 0)
+        {
+            vel.x = -moveSpeed;
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else if (xAxis > 0)
+        {
+            vel.x = moveSpeed;
+            transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            vel.x = 0;
+        }
+
+        if(isGrounded && isCrouching)
+        {
+            ChangeAnimationState(PLAYER_CROUCH);
+        }
+        if (isGrounded && !isAttacking && !isCrouching)
+        {
+            if (xAxis != 0)
+            {
+                ChangeAnimationState(PLAYER_WALK);
+            }
+            else
+            {
+                ChangeAnimationState(PLAYER_IDLE);
+            }
+        }
+
+        if (isJumpPressed && isGrounded && !isCrouching)
+        {
+            rb2d.AddForce(new Vector2(0, jumpForce));
+            isJumpPressed = false;
+            ChangeAnimationState(PLAYER_JUMP);
+        }
+
+        rb2d.velocity = vel;
+
+        if (isAttackPressed)
+        {
+            isAttackPressed = false;
+            if (!isAttacking)
+            {
+                isAttacking = true;
+                if (isGrounded)
+                {
+                    ChangeAnimationState(PLAYER_ATTACK);
+                }
+                else
+                {
+                    ChangeAnimationState(PLAYER_JUMPATTACK);
+                }
+
+                Invoke("AttackComplete", attackDelay);
+            }
+        }
+    }
+
+    void AttackComplete()
+    {
+        isAttacking = false;
+    }
 
     private void ChangeAnimationState(string newState)
     {
